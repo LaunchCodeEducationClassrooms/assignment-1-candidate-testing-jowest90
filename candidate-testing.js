52026a5d620db874ae2012cbd2af68fcb08fc70f
John West <jowest1990@gmail.com>
2021-02-15 19:14:56 +0000

    added a name in candiateName

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,7 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 // const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
-let candidateName = "";
+let candidateName = "Jasmine";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
