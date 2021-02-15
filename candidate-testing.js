fe5c0701381365f0d208c737ad40327fc5646498
John West <jowest1990@gmail.com>
2021-02-15 18:51:42 +0000

    candidateName=candidateName.toString();

--- candidate-testing.js
+++ candidate-testing.js
@@ -24,7 +24,8 @@ let candidateAnswers =[];
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   const input = require('readline-sync');
-  candidateName = input.question("Candidate Name: ").toString();
+  candidateName = input.question("Candidate Name: ");
+  candidateName=candidateName.toString();
 }
 
 function askQuestion() {
