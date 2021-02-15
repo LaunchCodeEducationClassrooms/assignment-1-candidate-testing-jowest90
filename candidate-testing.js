4a33410148d01015d92ab26abcd6ed88de5300bb
John West <jowest1990@gmail.com>
2021-02-15 18:25:39 +0000

    created quiz

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,37 +1,66 @@
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
-
+// const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
+let question = "1) Who was the first American woman in space?";
+let correctAnswer = "Sally Ride";
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions= ["1) Who was the first American woman in space?",
+                "2) True or false: 5000 meters = 5 kilometers. ", 
+                "3) (5 + 3)/2 * 10 = ?",
+                "2) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
+                "5) What is the minimum crew size for the International Space Station (ISS)?"];
+let correctAnswers = ["Sally Ride",
+                      "true", 
+                      "40",
+                      "Trajectory",
+                      "3"];
+let candidateAnswers =[];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  const input = require('readline-sync');
+  candidateName = input.question("Candidate Name: ");
 }
 
 function askQuestion() {
+  // console.log(candidateName);
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  const input = require('readline-sync');
+  for(i=0; i<questions.length; i++){
+    console.log(questions[i]+"\nYour Awnser: ");
+    candidateAnswers.push(input.question());
+    console.log("Correct Answer: "+correctAnswers[i]);
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
+  let grade=0;
+  for(i=0; i<questions.length; i++){
+  if(correctAnswers[i].toString().toUpperCase()===candidateAnswers[i].toString().toUpperCase()){
+    console.log("CORRECT!");
+    grade+=1;
+  }else{
+    console.log("WRONG!!");
+    grade+=0;
+  }
+  }
 
-  let grade;
-  
+  let response=grade;
+  grade = (grade/questions.length)*100;
 
+  console.log(">>> Overall Grade: "+grade+"% ("+response+" of 5 responses correct)  <<<");
+  if(grade < 60){
+    console.log(">>> Status: FAILED <<<");
+  }else{
+    console.log(">>> Status: PASSED <<<");
+  }
   return grade;
 }
 
